608b8db6aa419759b90a644775c35dc76810db56
Dil <126651350+SAGAR1215g@users.noreply.github.com>
2023-05-08 19:11:57 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -922,7 +922,7 @@ def main():
     migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
 
     # dispatcher.add_handler(test_handler)
-    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(rstart_handler)
     dispatcher.add_handler(help_handler)
     dispatcher.add_handler(about_callback_handler)
     dispatcher.add_handler(Music_callback_handler)
