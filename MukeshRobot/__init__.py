1c9d657d7b4c10e72a746974f024ddbb2e64cf09
Dil <126651350+SAGAR1215g@users.noreply.github.com>
2023-05-08 18:54:26 +0530

    Update __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -35,8 +35,8 @@ ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
 
-    API_ID = int(os.environ.get("API_ID", None))
-    API_HASH = os.environ.get("API_HASH", None)
+    API_ID = int(os.environ.get("API_ID", "21944949"))
+    API_HASH = os.environ.get("API_HASH", "d1abedd1624be80538e5d6da730f7963")
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
     ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
     CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
