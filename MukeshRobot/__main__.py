368c62177fbb7946c619943f939b71955a344b1d
Dil <126651350+SAGAR1215g@users.noreply.github.com>
2023-05-08 20:21:08 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -947,6 +947,6 @@ def main():
 
 if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
-    telethn.start(bot_token=TOKEN)
-    pbot.start()
+    telethn.rstart(bot_token=TOKEN)
+    pbot.rstart()
     main()
